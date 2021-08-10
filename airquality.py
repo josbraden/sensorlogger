c1f1a2353b16a999bb92374bf3253029acbddc8d
Josh Braden <josbraden@gmail.com>
2021-08-10 18:27:12 -0500

    Add testmysql function

--- airquality.py
+++ airquality.py
@@ -17,6 +17,31 @@ dbcompress = False
 dbcharset = "utf8mb4"
 dbcollation = "utf8mb4_general_ci"
 
+# Function to test database connection
+def testMySQL():
+    print("Testing database connection...", end='')
+    try:
+        connection = mysql.connector.connect(
+            host=dbhost, user=dbuser, passwd=dbpasswd,
+            database=dbschema, compress=dbcompress)
+
+    except mysql.connector.Error as err:
+        print(err)
+        return -1
+
+    else:
+        connection.set_charset_collation(dbcharset, dbcollation)
+        connectioncursor = connection.cursor()
+        connectioncursor.execute("SELECT 0")
+        connresult = connectioncursor.fetchone()
+        for i in connresult:
+            print("Success")
+
+        connectioncursor.close()
+        connection.close()
+        return 0
+
+
 # Function to read data from sensor
 def readsensor():
     ser = serial.Serial('/dev/ttyUSB0')
