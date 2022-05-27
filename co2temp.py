c73b21afd6e0c1adb1ce7b02d0f963c01f3d3afb
Josh Braden <josbraden@gmail.com>
2022-05-27 14:49:32 -0500

    Fix CI

--- co2temp.py
+++ co2temp.py
@@ -1,8 +1,8 @@
 # Logs C02 with help from the vfilimonov/co2meter library
 # Also logs temperature since that data is recorded with the same instrument
 # Device used: CO2Meter RAD-0301 Mini
-# Raw device info: {'vendor_id': 1241, 'product_id': 41042, 
-# 'path': b'1-1.1:1.0', 'manufacturer': 'Holtek', 
+# Raw device info: {'vendor_id': 1241, 'product_id': 41042,
+# 'path': b'1-1.1:1.0', 'manufacturer': 'Holtek',
 # 'product_name': 'USB-zyTemp', 'serial_no': '2.00'}
 # @author Josh Braden
 
