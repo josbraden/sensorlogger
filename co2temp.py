1254aa02e7d2bf40edafe5244e8832a4bd7a8070
Josh Braden <josbraden@gmail.com>
2022-05-27 14:46:30 -0500

    Add main function and CI fixes

--- co2temp.py
+++ co2temp.py
@@ -1,7 +1,9 @@
 # Logs C02 with help from the vfilimonov/co2meter library
 # Also logs temperature since that data is recorded with the same instrument
 # Device used: CO2Meter RAD-0301 Mini
-# Raw device info: {'vendor_id': 1241, 'product_id': 41042, 'path': b'1-1.1:1.0', 'manufacturer': 'Holtek', 'product_name': 'USB-zyTemp', 'serial_no': '2.00'}
+# Raw device info: {'vendor_id': 1241, 'product_id': 41042, 
+# 'path': b'1-1.1:1.0', 'manufacturer': 'Holtek', 
+# 'product_name': 'USB-zyTemp', 'serial_no': '2.00'}
 # @author Josh Braden
 
 import time
@@ -70,3 +72,18 @@ def readsensor():
     mon = co2meter.CO2monitor()
     data = mon.read_data()
     return insertData(data[1], data[2])
+
+
+# Main function
+while True:
+    ret = readsensor()
+    if ret == 0:
+        time.sleep(pollinterval)
+
+    elif ret == 1:
+        print("Ignored a large sensor value")
+        time.sleep(pollinterval)
+
+    else:
+        print("Error logging data")
+        time.sleep(failSleep)
